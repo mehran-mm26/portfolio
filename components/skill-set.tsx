32f6a83eeb0e1a7404c8e20ce6638d4a46e60476
Mehran <m.mahmoudkhani@k2-systems.de>
2025-11-12 22:52:36 +0100

    fix: change article tag to div

--- components/skill-set.tsx
+++ components/skill-set.tsx
@@ -5,7 +5,7 @@ const SkillSet: React.FC<{ skillSet: ISkillSet }> = ({
   skillSet: { title, description, icons },
 }) => {
   return (
-    <article className=' border border-gray-300 rounded-[10px] p-6 bg-white flex flex-col sm:w-[47%] md:flex-1'>
+    <div className=' border border-gray-300 rounded-[10px] p-6 bg-white flex flex-col sm:w-[47%] md:flex-1'>
       <h2 className=' text-gray-800 mb-4'>{title}</h2>
       <p className=' text-sm font-normal mb-6 text-slate-500'>{description}</p>
 
@@ -22,7 +22,7 @@ const SkillSet: React.FC<{ skillSet: ISkillSet }> = ({
             />
           ))}
       </div>
-    </article>
+    </div>
   );
 };
 
